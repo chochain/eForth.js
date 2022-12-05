228851291ec3847ba43b771b24b8071eea8bf2ae
Chochain Lee <chochain@yahoo.com>
2022-12-05 15:20:50 -0800

    ~/modules/debug.js access vm directly

--- modules/debug.js
+++ modules/debug.js
@@ -5,35 +5,35 @@ import { log, spaces } from './io.js'
 const DUMP_WIDTH = 54                               ///< dump control
 const CR = "\n"
 
-export const words = (dict)=>{                      ///< word op
+export const words = (vm)=>{                        ///< word op
     let sz = 0
-    dict.forEach((w,i)=>{                           /// * loop thru all words
-        log(w.name+' ')
+    vm.dict.forEach((w,i)=>{                        /// * loop thru all words
+        vm.log(w.name+' ')
         sz += w.name.length + 1
-        if (sz > DUMP_WIDTH) { log(CR); sz = 0 }
+        if (sz > DUMP_WIDTH) { vm.log(CR); sz = 0 }
     })
-    log(CR)
+    vm.log(CR)
 }
-export const dump = (dict, n0, n1)=>{               ///< memory dump op
+export const dump = (vm, n0, n1)=>{                ///< memory dump op
     for (let i = n0; i <= n1; i++) {
-        let w = dict[i]
-        log('dict[' + i + ']=("' + w.name + '" ')
-        if (w.xt) log(w.xt)
-        else      log(', pf[' + w.pf.map(w1=>w1.name) + ']')
-        if (w.qf) log(', qf[' + w.qf + ']')
-        log((w.immd ? 'immd)' : ')') + log(CR))
+        let w = vm.dict[i]
+        vm.log('dict[' + i + ']=("' + w.name + '" ')
+        if (w.xt) vm.log(w.xt)
+        else      vm.log(', pf[' + w.pf.map(w1=>w1.name) + ']')
+        if (w.qf) vm.log(', qf[' + w.qf + ']')
+        vm.log((w.immd ? 'immd)' : ')') + vm.log(CR))
     }
 }
-export const see = (w, n=0)=>{                      ///< see op
+export const see = (vm, w, n=0)=>{                  ///< see op
     const _show_pf = (pf)=>{
         if (pf == null || pf.length == 0) return
-        log("["+CR); spaces(2*(n+1))                /// * indent section
+        vm.log("["+CR); spaces(2*(n+1))             /// * indent section
         pf.forEach(w=>see(w, n+1))                  /// * recursive call
         log('] ')                                   /// * close section
     }
-    log(w.name+' ')                                 /// * display word name
+    vm.log(w.name+' ')                              /// * display word name
     if (w.qf != null && w.qf.length > 0) {          /// * display qf array
-        log('='+JSON.stringify(w.qf)+' ')
+        vm.log('='+JSON.stringify(w.qf)+' ')
     }
     _show_pf(w.pf)                                  /// * if.{pf}, for.{pf}, or begin.{pf}
     _show_pf(w.pf1)                                 /// * else.{pf1}.then, or .then.{pf1}.next
