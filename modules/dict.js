e0754daebe6bee87acc31beb64277b08baffc550
chochain <chochain@yahoo.com>
2022-12-05 23:08:58 -0800

    ~/modules/dict.js call vm.log; fix mem access words

--- modules/dict.js
+++ modules/dict.js
@@ -32,12 +32,11 @@ export const init = (vm)=>{                              ///< dictionary constru
     /// @{
     let vm_add = (compi=false)=>{
         let s = io.nxtok();                              ///< fetch an input token
+        if (s==null) { this.compi=false; throw "more input" }
         vm.add(s)
         vm.compi = compi
     }
     let vm_nvar = (xt, v)=>{ vm_add(); vm.nvar(xt, v) }
-    let vm_tail = (i=1)  =>{ return vm.dict[vm.dict.length - i] } ///< last entry
-    let vm_last = ()     =>{ return vm_tail(2).pf.tail() }        ///< pf of last word created
     /// @}
     [
         /// @defgroup Stack ops
@@ -114,34 +113,34 @@ export const init = (vm)=>{                              ///< dictionary constru
         new Immd("s\"",   "li", c=>{
             let s = io.nxtok('"')
             if (vm.compi) vm.compile("dostr", s)
-            else        push(s)                        /// * push string object
+            else push(s)                               /// * push string object
         }),
         new Immd(".\"",   "li", c=>vm.compile("dolit", io.nxtok('"'))),
         new Immd("(",     "li", c=>io.nxtok(')')),
-        new Immd(".(",    "li", c=>io.log(io.nxtok(')'))),
+        new Immd(".(",    "li", c=>vm.log(io.nxtok(')'))),
         new Immd("\\",    "li", c=>io.clear()),
         /// @}
         /// @defgroup Branching - if.{pf}.then, if.{pf}.else.{pf1}.then
         /// @{
         new Immd("if",    "br", c=>br._if(vm)),
-        new Immd("else",  "br", c=>br._else(vm.dict)),
-        new Immd("then",  "br", c=>br._then(vm.dict)),
+        new Immd("else",  "br", c=>br._else(vm)),
+        new Immd("then",  "br", c=>br._then(vm)),
         /// @}
         /// @defgroup Loop ops
         /// @brief begin.{pf}.again, begin.{pf}.until, begin.{pf}.while.{pf1}.repeat
         /// @{
         new Immd("begin", "br", c=>br._begin(vm)),
-        new Immd("while", "br", c=>br._while(dict)),
-        new Immd("repeat","br", c=>br._repeat(dict)),
-        new Immd("again", "br", c=>br._again(dict)),
-        new Immd("until", "br", c=>br._until(dict)),
+        new Immd("while", "br", c=>br._while(vm)),
+        new Immd("repeat","br", c=>br._repeat(vm)),
+        new Immd("again", "br", c=>br._again(vm)),
+        new Immd("until", "br", c=>br._until(vm)),
         /// @}
         /// @defgroup Loop ops
         /// @brief for.{pf}.next, for.{pf}.aft.{pf1}.then.{pf2}.next
         /// @{
         new Immd("for",   "br", c=>br._for(vm)),
-        new Immd("aft",   "br", c=>br._aft(dict)),
-        new Immd("next",  "br", c=>br._next(dict)),
+        new Immd("aft",   "br", c=>br._aft(vm)),
+        new Immd("next",  "br", c=>br._next(vm)),
         new Prim(">r",    "br", c=>vm.rs.push(pop())),   /// * push into rstack
         new Prim("r>",    "br", c=>push(vm.rs.pop())),   /// * pop from rstack
         new Prim("r@",    "br", c=>push(rtop())),        /// * fetch from rstack
@@ -150,44 +149,53 @@ export const init = (vm)=>{                              ///< dictionary constru
         /// @}
         /// @defgroup Memory Access ops
         /// @{
-        new Prim("?",     "mm", c=>io.log(mm.at(dict[pop()]))),
-        new Prim("@",     "mm", c=>push(mm.at(dict[pop()]))),                              // w -- n
-        new Prim("!",     "mm", c=>{ let w=pop(); mm.store(dict[w], pop()) }),             // n w  --
-        new Prim("+!",    "mm", c=>{ let w=pop(); mm.storeplus(dict[w], pop()) }),         // n w --
+        new Prim("?",     "mm", c=>vm.log(mm.at(vm.dict[pop()]))),
+        new Prim("@",     "mm", c=>push(mm.at(vm.dict[pop()]))),                           // w -- n
+        new Prim("!",     "mm", c=>{ let w=pop(); mm.store(vm.dict[w], pop()) }),          // n w  --
+        new Prim("+!",    "mm", c=>{ let w=pop(); mm.storeplus(vm.dict[w], pop()) }),      // n w --
         new Prim("allot", "mm", c=>{                                                       // n --
             vm.nvar(_dovar, 0)                                                             // create qf array
-            for (let n=pop(), i=1; i<n; i++) mm.arr_store(vm_tail(), i, 0)
+            for (let n=pop(), i=1; i<n; i++) mm.arr_store(vm.tail(), i, 0)
         }),
-        new Prim("n?",    "mm", c=>{ let i=pop(); io.log(mm.arr_at(dict[pop()], i)) }),                 // w i --
-        new Prim("n@",    "mm", c=>{ let i=pop(); let w = pop(); push(mm.arr_at(dict[w], i)) }),        // w i -- n
-        new Prim("n!",    "mm", c=>{ let i=pop(); let w = pop(); mm.arr_store(dict[w], i, pop()) }),    // n w i --
+        new Prim("n?",    "mm", c=>{                                       // w i --
+			let i=pop()
+			vm.log(mm.arr_at(vm.dict[pop()], i))
+		}),
+        new Prim("n@",    "mm", c=>{                                       // w i -- n
+			let i=pop(), w = pop()
+			push(mm.arr_at(vm.dict[w], i))
+		}),
+        new Prim("n!",    "mm", c=>{                                       // n w i --
+			let i=pop(), w = pop()
+			mm.arr_store(vm.dict[w], i, pop())
+		}),
         /// @}
         /// @defgroup Word Defining ops
         /// @{
         new Immd("exec",     "cm", c=>vm.dict[pop()].exec()),
-        new Prim(":",        "cm", c=>vm_add(true)),                       ///< fetch an input token
+        new Prim(":",        "cm", c=>vm_add(true)),                       // fetch an input token
         new Immd(";",        "cm", c=>vm.compi=false),                     // semicolon
         new Immd("variable", "cm", c=>vm_nvar(_dovar, 0)),
         new Immd("constant", "cm", c=>vm_nvar(_docon, pop())),
         new Prim("create",   "cm", c=>vm_add()),                           // create new word
         new Prim(",",        "cm", c=>{                                    // push TOS into qf
-            let pf = vm_tail().pf
+            let pf = vm.tail().pf
             if (pf.length) pf[0].qf.push(pop())                            // append more values
             else           vm.nvar(_dovar, pop())                          // 1st value in qf
         }),
         new Prim("allot",    "cm", c=>{                                    // n --
             nvar(_dovar, 0)                                                // create qf array
-            for (let n=pop(), i=1; i<n; i++) vm_tail().val[i]=0 }),        // fill all slot with 0
+            for (let n=pop(), i=1; i<n; i++) vm.tail().val[i]=0 }),        // fill all slot with 0
         new Prim("does",     "cm", c=>does(vm.dict)),
         new Prim("to",       "cm", c=>tok2w().val[0]=pop()),               // update constant
         new Prim("is",       "cm", c=>{                                    // n -- alias a word
             vm_add()    
-            vm_tail().pf = vm.dict[pop()].pf
+            vm.tail().pf = vm.dict[pop()].pf
         }),
         /// @}
         /// @defgroup Debugging ops
         /// @{
-        new Prim("here",     "db", c=>push(vm_tail().token)),
+        new Prim("here",     "db", c=>push(vm.tail().token)),
         new Prim(".s",       "db", c=>io.dot(vm.ss)),
         new Prim("words",    "db", c=>debug.words(vm)),
         new Prim("dump",     "db", c=>{ let n=pop(); debug.dump(vm, pop(), n) }),
@@ -198,13 +206,14 @@ export const init = (vm)=>{                              ///< dictionary constru
         /// @{
         new Prim("clock",    "os", c=>push(os.clock())),
         new Prim("delay",    "os", c=>os.sleep(pop())),
-        new Prim("date",     "os", c=>io.log(os.date()+" ")),
-        new Prim("time",     "os", c=>io.log(os.time()+" ")),
+        new Prim("date",     "os", c=>vm.log(os.date()+" ")),
+        new Prim("time",     "os", c=>vm.log(os.time()+" ")),
         new Prim("eval",     "os", c=>os.js_eval(pop())),           /// * dangerous, be careful!
         /// @}
         new Prim("boot",     "os", c=>{
             let b = vm.find("boot")                                 /// * purge everything upto 'boot'
-            purge(dict, b, b)
+            purge(vm.dict, b, b)
             vm.reset()
-        })].forEach(c=>vm.dict.push(c))
+        })
+	].forEach(c=>vm.dict.push(c))
 }
