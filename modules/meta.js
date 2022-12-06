ea21c5790c59b93c2694292cbbac196dc90857a4
Chochain Lee <chochain@yahoo.com>
2022-12-06 10:59:42 -0800

    ~/modules/meta.js mv _docon, _dovar from dict.js

--- modules/meta.js
+++ modules/meta.js
@@ -1,9 +1,20 @@
-
+import { Prim, Immd } from './core.js'
+/// @defgroup Dictionary access methods
+/// @{
+/// @}
+/// @defgroup Word Defining ops
+/// @{
 export const voc = (vm)=>{
+    const _docon = c=>vm.ss.push(c.qf[0])                                  ///< constant op
+    const _dovar = c=>vm.ss.push(c.token)                                  ///< variable op
+    const vm_add = (compi=false)=>{
+        let s = vm.nxtok();                                                ///< fetch an input token
+        if (s==null) { this.compi=false; throw "more input" }
+        vm.add(s)
+        vm.compi = compi
+    }
+    const vm_nvar = (xt, v)=>{ vm_add(); vm.nvar(xt, v) }
     return [
-        /// @}
-        /// @defgroup Word Defining ops
-        /// @{
         new Immd("exec",     "cm", c=>vm.dict[pop()].exec()),
         new Prim(":",        "cm", c=>vm_add(true)),                       // fetch an input token
         new Immd(";",        "cm", c=>vm.compi=false),                     // semicolon
@@ -26,3 +37,4 @@ export const voc = (vm)=>{
         })
     ]
 }
+/// @}
