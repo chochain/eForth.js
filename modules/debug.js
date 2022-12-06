019458d6b7ee7aff497ba700df757714e6997551
chochain <chochain@yahoo.com>
2022-12-05 23:07:46 -0800

    ~/modules/debug.js use log=vm.log (short-hand)

--- modules/debug.js
+++ modules/debug.js
@@ -4,32 +4,34 @@ const DUMP_WIDTH = 54                               ///< dump control
 const CR = "\n"
 
 export const words = (vm)=>{                        ///< word op
-    let sz = 0
+	let log = vm.log
+    let sz  = 0
     vm.dict.forEach((w,i)=>{                        /// * loop thru all words
-        vm.log(w.name+' ')
+        log(w.name+' ')
         sz += w.name.length + 1
-        if (sz > DUMP_WIDTH) { vm.log(CR); sz = 0 }
+        if (sz > DUMP_WIDTH) { log(CR); sz = 0 }
     })
-    vm.log(CR)
+    log(CR)
 }
 export const dump = (vm, n0, n1)=>{                ///< memory dump op
+	let log = vm.log
     for (let i = n0; i <= n1; i++) {
         let w = vm.dict[i]
-        vm.log('dict[' + i + ']=("' + w.name + '" ')
-        if (w.xt) vm.log(w.xt)
-        else      vm.log(', pf[' + w.pf.map(w1=>w1.name) + ']')
-        if (w.qf) vm.log(', qf[' + w.qf + ']')
-        vm.log((w.immd ? 'immd)' : ')') + vm.log(CR))
+        log('dict[' + i + ']=("' + w.name + '" ')
+        if (w.xt) log(w.xt)
+        else      log(', pf[' + w.pf.map(w1=>w1.name) + ']')
+        if (w.qf) log(', qf[' + w.qf + ']')
+        log((w.immd ? 'immd)' : ')') + log(CR))
     }
 }
 export const see = (vm, w, n=0)=>{                  ///< see op
     const _indent = (n)=>{
-        for (let i=0; i < 2*(n+1); i++) vm.log(' ')
+        for (let i=0; i < 2*n; i++) vm.log(' ')
     }
     const _show_pf = (pf)=>{
         if (pf == null || pf.length == 0) return
-        vm.log("["+CR); _indent()                   /// * indent section
-        pf.forEach(w=>see(w, n+1))                  /// * recursive call
+        vm.log("["+CR); _indent(n+1)                /// * indent section
+        pf.forEach(w=>this.see(vm, w, n+1))         /// * recursive call
         vm.log('] ')                                /// * close section
     }
     vm.log(w.name+' ')                              /// * display word name
