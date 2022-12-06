96633580b3118648e3a0cf9ecb227e575d66218e
Chochain Lee <chochain@yahoo.com>
2022-12-06 11:40:26 -0800

    ~/modules/io.js use vm.base directly; mv short-hand into voc

--- modules/io.js
+++ modules/io.js
@@ -5,28 +5,9 @@ import { BOOL, ZERO, Prim } from './core.js'
 const SPC  = ' ', CR="\n"                                   ///< string constants
 
 let _out   = console.log
-let _base  = 10                                             ///< numeric radix
-let _ucase = false                                          ///< case sensitivity
 let _tib   = "", _ntib = 0                                  ///< input buffer
 
-export const set_base = (b)=>_base=(b | 0)
-export const get_base = () =>{ return _base | 0 }
 export const set_tib  = (r)=>{ _tib=r + SPC; _ntib=0 }      ///< capture into TIB
-
-export const log      = (s)=>_out(s)                        ///< output port
-export const key      = () =>{ return io.nxtok()[0] }
-export const emit     = (c)=>log(String.fromCharCode(c))
-export const dot      = (n)=>{
-    let a = typeof(n)!='object' ? [ n ] : n
-    log(a.map(v=>v.toString(_base)).join(SPC)+SPC)
-}
-export const dot_r    = (n, v)=>{
-    let s = v.toString(_base)
-    for(let i=0; i+s.length < n; i++) log(SPC)
-    log(s)
-}
-export const cr       = () =>log(CR)
-export const spaces   = (n)=>{ for (let i=0; i<n; i++) log(SPC) }
 export const clear    = () =>_ntib=_tib.length              ///< clear input buffer
 export const nxtok    = (d=SPC)=>{                          ///< assumes tib ends with a blank
     while (d==SPC &&
@@ -36,16 +17,31 @@ export const nxtok    = (d=SPC)=>{                          ///< assumes tib end
     return s
 }
 export const init = (output=console.log)=>_out=output
+export const log  = (s)=>_out(s)                            ///< output port
+
 export const voc  = (vm)=>{
-    const push = v=>vm.ss.push(v)
+    const push   = v=>vm.ss.push(v)
+    const pop    = ()=>{ return vm.ss.pop() }
+    const key    = () =>{ return nxtok()[0] }
+    const emit   = (c)=>log(String.fromCharCode(c))
+    const spaces = (n)=>{ for (let i=0; i<n; i++) log(SPC) }
+    const dot    = (n)=>{
+        let a = typeof(n)!='object' ? [ n ] : n
+        log(a.map(v=>v.toString(vm.base)).join(SPC)+SPC)
+    }
+    const dot_r= (n, v)=>{
+        let s = v.toString(vm.base)
+        for(let i=0; i+s.length < n; i++) log(SPC)
+        log(s)
+    }
     return [
         /// @defgroup IO ops
         /// @{
-        new Prim("ucase!","io", c=>_ucase=BOOL(ZERO(pop()))),
+        new Prim("ucase!","io", c=>vm.ucase=BOOL(ZERO(pop()))),
         new Prim("base@", "io", c=>push(get_base())),
-        new Prim("base!", "io", c=>set_base(pop())),
-        new Prim("hex",   "io", c=>set_base(16)),
-        new Prim("decimal","io",c=>set_base(10)),
+        new Prim("base!", "io", c=>vm.base = pop() | 0),
+        new Prim("hex",   "io", c=>vm.base = 16),
+        new Prim("decimal","io",c=>vm.base = 10),
         new Prim("cr",    "io", c=>log(CR)),
         new Prim(".",     "io", c=>dot(pop())),
         new Prim(".r",    "io", c=>{ let n=pop(); dot_r(n, pop()) }),
