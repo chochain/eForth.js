1eae6b7c1afe0ff8cd6935ab465b289f3147b547
Chochain Lee <chochain@yahoo.com>
2022-12-09 16:11:44 +0800

    ~/modules/vm.js rework branching methods

--- modules/vm.js
+++ modules/vm.js
@@ -40,17 +40,17 @@ export class VM {
         this.compi = this.ucase = false
         this.base = 10
     }
-    add(s)    { this.dict.push(new Code(s, true)) } ///< add a word to dictionary
-    extend(d) { d.forEach(c=>this.dict.push(c)) }   ///< extending dictionary
+    add(s)    { this.dict.push(new Code(s, null, true)) } ///< add a word to dictionary
+    extend(d) { d.forEach(c=>this.dict.push(c)) }         ///< extending dictionary
     comma(w)  {
 		this.dict[this.dict.length - 1].pf.push(w)  ///< compile w into pf[]
     }
-    compile(s, v, xt=null) {               ///< compile a word
-        let w = new Code(s, v, xt==null ? this.find(s).xt : xt)
+    compile(s, xt=null, v=false) {                  ///< compile a word
+        let w = new Code(s, xt!=null ? xt : this.find(s).xt, v)
         this.comma(w)
     }
     nvar(xt, v) {
-        this.compile("dovar", v)
+        this.compile("dovar", xt, v)
         let t   = this.dict[this.dict.length-i]
         let w   = t.pf[0]                  ///< last work and its pf
         t.val   = w.qf                     /// * create a val func
@@ -99,31 +99,32 @@ export class VM {
             this.compi=false               ///>> restore interpret mode
         }
         else if (cc) {                     ///> in compile mode?
-            this.compile("dolit", n)       ///>> compile the number
+            this.compile("dolit", null, n) ///>> compile the number
         }
         else this.ss.push(n)               ///>> or, push number onto stack top
     }
-    /// @defgroup Built-in (branching, looping) functions
+    /// @defgroup Built-in for branching functions
     /// @{
-    _dec_i()  { this.rs[this.rs.length - 1] -= 1 }      ///< decrement I
-    _bran(c)  { run(ZERO(pop()) ? c.pf1 : c.pf) }       ///< branch op
-    _for(c) {                                           ///< for..loop op
+    dec_i()  { return (this.rs[this.rs.length - 1] -= 1) } ///< decrement I
+    bran(c)  { run(ZERO(this.ss.pop()) ? c.pf1 : c.pf) }   ///< branch op
+    loop(c) {                                              ///< for..loop op
+        console.log(c)
         do { run(c.pf) }
-        while (c.stage==0 && this._dec_i() >= 0)        ///< for.{pf}.next only
-        while (c.stage>0) {                             /// * aft
-            run(c.pf2)                                  /// * aft.{pf2}.next
-            if (this._dec_i() < 0) break
-            run(c.pf1)                                  /// * then.{pf1}.next
+        while (c.stage==0 && this.dec_i() >= 0)            ///< for.{pf}.next only
+        while (c.stage>0) {                                /// * aft
+            run(c.pf2)                                     /// * aft.{pf2}.next
+            if (this.dec_i() < 0) break
+            run(c.pf1)                                     /// * then.{pf1}.next
         }
-        this.rs.pop()                                   /// * pop off I
+        this.rs.pop()                                      /// * pop off I
     }
-    _loop(c) {                                          ///< begin..util op
+    cycle(c) {                                              ///< begin..util op
         while (true) {
-            run(c.pf)                                   /// * begin.{pf}.
-            if (c.stage==0 && INT(pop())!=0) break      /// * until
-            if (c.stage==1) continue                    /// * again
-            if (c.stage==2 && ZERO(pop())) break        /// * while
-            run(c.pf1)                                  /// * .{pf1}.until
+            run(c.pf)                                      /// * begin.{pf}.
+            if (c.stage==0 && INT(pop())!=0) break         /// * until
+            if (c.stage==1) continue                       /// * again
+            if (c.stage==2 && ZERO(pop())) break           /// * while
+            run(c.pf1)                                     /// * .{pf1}.until
         }
     }
     /// @}
