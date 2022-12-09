a5274c92839a7e9e9e8ea92a89bf29cc8e79d84c
Chochain Lee <chochain@yahoo.com>
2022-12-08 17:53:35 -0800

    ~/modules/eforth.js mv exports to front

--- modules/eforth.js
+++ modules/eforth.js
@@ -5,38 +5,6 @@
 ///   > let vm = new Forth(), or
 ///   > let vm = Forth()
 ///
-import { Prim, purge } from './core.js'
-import * as au  from './alu.js'
-import * as io  from './io.js'
-import * as li  from './literal.js'
-import * as br  from './branch.js'
-import * as mm  from './mem.js'
-import * as me  from './meta.js'
-import * as db  from './debug.js'
-import * as os  from './os.js'
-import { VM }   from './vm.js'
-
-///======================================================================
-/// dictionary intialized with primitive words
-///
-const dict_setup = (vm)=>{           ///< dictionary constructor
-    vm.extend(au.voc(vm))            /// * ALU
-    vm.extend(io.voc(vm))            /// * IO
-    vm.extend(li.voc(vm))            /// * Literal
-    vm.extend(br.voc(vm))            /// * Branching
-    vm.extend(mm.voc(vm))            /// * Memory
-    vm.extend(me.voc(vm))            /// * Meta compiler
-    vm.extend(db.voc(vm))            /// * Debug
-    vm.extend(os.voc(vm))            /// * OS
-    vm.extend([                      /// * last fence
-        new Prim("boot", "os", c=>{  /// * purge all user words (i.e. upto 'boot')
-            let b = vm.find("boot")
-            purge(vm.dict, b, b)
-            vm.reset()
-        })
-	])
-}
-///
 /// interfaces to Javascript
 ///
 export { default as embed_forth } from './embed.js'   ///< embedded Forth listener
@@ -65,5 +33,35 @@ export function Forth(output=console.log) {
         exec: exec                         ///< outer interpreter
     }
 }
+///======================================================================
+/// dictionary intialized with primitive words
+///
+import { Prim, purge } from './core.js'
+import * as au  from './alu.js'
+import * as io  from './io.js'
+import * as li  from './literal.js'
+import * as br  from './branch.js'
+import * as mm  from './mem.js'
+import * as me  from './meta.js'
+import * as db  from './debug.js'
+import * as os  from './os.js'
+import { VM }   from './vm.js'
 
+const dict_setup = (vm)=>{           ///< dictionary constructor
+    vm.extend(au.voc(vm))            /// * ALU
+    vm.extend(io.voc(vm))            /// * IO
+    vm.extend(li.voc(vm))            /// * Literal
+    vm.extend(br.voc(vm))            /// * Branching
+    vm.extend(mm.voc(vm))            /// * Memory
+    vm.extend(me.voc(vm))            /// * Meta compiler
+    vm.extend(db.voc(vm))            /// * Debug
+    vm.extend(os.voc(vm))            /// * OS
+    vm.extend([                      /// * last fence
+        new Prim("boot", "os", c=>{  /// * purge all user words (i.e. upto 'boot')
+            let b = vm.find("boot")
+            purge(vm.dict, b, b)
+            vm.reset()
+        })
+	])
+}
 
