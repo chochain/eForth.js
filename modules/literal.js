f048e23294cbae9bd8a26b9a81113719f24a26f8
Chochain Lee <chochain@yahoo.com>
2022-12-08 19:04:11 -0800

    ~/modules/literal.js fix push macro

--- modules/literal.js
+++ modules/literal.js
@@ -1,7 +1,7 @@
 import { Prim, Immd } from './core.js'
 
 export const voc = (vm)=>{
-    const push = v=>vm.ss.push()
+    const push = v=>vm.ss.push(v)                      ///< macro
     return [
         /// @defgroup Literal ops
         /// @{
@@ -9,7 +9,10 @@ export const voc = (vm)=>{
         new Prim("dostr", "li", c=>push(c.token)),     /// * string literal token
         new Immd("[",     "li", c=>vm.compi=false ),
         new Prim("]",     "li", c=>vm.compi=true ),
-        new Prim("'",     "li", c=>{ let w=vm.tok2w(); push(w.token) }),
+        new Prim("'",     "li", c=>{
+            let w = vm.tok2w()
+            if (w!=null) push(w.token)
+        }),
         new Immd("s\"",   "li", c=>{
             let s = vm.tok('"')
             if (vm.compi) vm.compile("dostr", s)
