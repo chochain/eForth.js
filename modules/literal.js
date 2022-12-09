b059ceaf3979f3143883d708f561c7f0babd3e91
Chochain Lee <chochain@yahoo.com>
2022-12-08 19:52:34 -0800

    ~/modules/literal.js fix s-quote call vm.xtib

--- modules/literal.js
+++ modules/literal.js
@@ -7,21 +7,21 @@ export const voc = (vm)=>{
         /// @{
         new Prim("dolit", "li", c=>push(c.qf[0])),     /// * integer literal or string
         new Prim("dostr", "li", c=>push(c.token)),     /// * string literal token
-        new Immd("[",     "li", c=>vm.compi=false ),
-        new Prim("]",     "li", c=>vm.compi=true ),
-        new Prim("'",     "li", c=>{
-            let w = vm.tok2w()
-            if (w!=null) push(w.token)
-        }),
         new Immd("s\"",   "li", c=>{
             let s = vm.tok('"')
-            if (vm.compi) vm.compile("dostr", s)
-            else push(s)                               /// * push string object
+            if (s != null) {
+                if (vm.compi) vm.compile("dostr", s)
+                else push(s)                           /// * push string object
+            }
+            else {
+                vm.log('one quote? ')
+                vm.xtib()                             /// * clear input buffer
+            }
         }),
         new Immd(".\"",   "li", c=>vm.compile("dolit", vm.tok('"'))),
         new Immd("(",     "li", c=>vm.tok(')')),
         new Immd(".(",    "li", c=>vm.log(vm.tok(')'))),
-        new Immd("\\",    "li", c=>io.clear()),
+        new Immd("\\",    "li", c=>vm.xtib()),
         /// @}
     ]
 }
