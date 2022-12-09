b4c1e14dcfea229441e4853dabcae3c58f7f47a6
Chochain Lee <chochain@yahoo.com>
2022-12-09 13:49:10 +0800

    ~/modules/debug.js fix _dump, _see, .s

--- modules/debug.js
+++ modules/debug.js
@@ -4,7 +4,7 @@ import { Prim, purge } from './core.js'
 const DUMP_WIDTH = 54                               ///< dump control
 const CR = "\n"
 
-const _words = (vm)=>{                        ///< word op
+const _words = (vm)=>{                              ///< word op
 	let log = vm.log
     let sz  = 0
     vm.dict.forEach((w,i)=>{                        /// * loop thru all words
@@ -14,25 +14,24 @@ const _words = (vm)=>{                        ///< word op
     })
     log(CR)
 }
-const _dump = (vm, n0, n1)=>{                ///< memory dump op
+const _dump = (vm, n0, n1)=>{                       ///< memory dump op
 	let log = vm.log
     for (let i = n0; i <= n1; i++) {
         let w = vm.dict[i]
-        log('dict[' + i + ']=("' + w.name + '" ')
+        log('['+i+(w.immd ? ']*=' : ']="') + w.name + '", ')
         if (w.xt) log(w.xt)
-        else      log(', pf[' + w.pf.map(w1=>w1.name) + ']')
-        if (w.qf) log(', qf[' + w.qf + ']')
-        log((w.immd ? 'immd)' : ')') + log(CR))
+        else      log('pf='+JSON.stringify(w.pf))   /// * user defined word
+        log(CR)
     }
 }
-const _see = (vm, w, n=0)=>{                  ///< see op
+const _see = (vm, w, n=0)=>{                        ///< see op
     const _indent = (n)=>{
         for (let i=0; i < 2*n; i++) vm.log(' ')
     }
     const _show_pf = (pf)=>{
         if (pf == null || pf.length == 0) return
         vm.log("["+CR); _indent(n+1)                /// * indent section
-        pf.forEach(w=>this.see(vm, w, n+1))         /// * recursive call
+        pf.forEach(w1=>_see(vm, w1, n+1))           /// * recursive call
         vm.log('] ')                                /// * close section
     }
     vm.log(w.name+' ')                              /// * display word name
@@ -51,9 +50,9 @@ export const voc = (vm) => {
         /// @defgroup Debugging ops
         /// @{
         new Prim("here",     "db", c=>push(vm.tail().token)),
-        new Prim(".s",       "db", c=>io.dot(vm.ss)),
+        new Prim(".s",       "db", c=>vm.log(JSON.stringify(vm.ss)+CR)),
         new Prim("words",    "db", c=>_words(vm)),
-        new Prim("dump",     "db", c=>{ let n=pop(); _dump(vm, pop(), n) }),
+        new Prim("dump",     "db", c=>{ let n1=pop(); _dump(vm, pop(), n1) }),
         new Prim("see",      "db", c=>{ let w=vm.tok2w(); console.log(w); _see(vm, w) }),
         new Prim("forget",   "db", c=>purge(vm.dict, vm.tok2w(), vm.find("boot"))),
         /// @}
