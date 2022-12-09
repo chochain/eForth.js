cfbed46a5aff1bce7d41f99b668c2f0cadcc731e
Chochain Lee <chochain@yahoo.com>
2022-12-08 17:50:36 -0800

    ~/modules/eforth.js merge in dict.js

--- modules/eforth.js
+++ modules/eforth.js
@@ -5,12 +5,40 @@
 ///   > let vm = new Forth(), or
 ///   > let vm = Forth()
 ///
-/// default module interface to JS engine
-///
-import * as io   from './io.js'
-import * as dict from './dict.js'
-import { VM }    from './vm.js'
+import { Prim, purge } from './core.js'
+import * as au  from './alu.js'
+import * as io  from './io.js'
+import * as li  from './literal.js'
+import * as br  from './branch.js'
+import * as mm  from './mem.js'
+import * as me  from './meta.js'
+import * as db  from './debug.js'
+import * as os  from './os.js'
+import { VM }   from './vm.js'
 
+///======================================================================
+/// dictionary intialized with primitive words
+///
+const dict_setup = (vm)=>{           ///< dictionary constructor
+    vm.extend(au.voc(vm))            /// * ALU
+    vm.extend(io.voc(vm))            /// * IO
+    vm.extend(li.voc(vm))            /// * Literal
+    vm.extend(br.voc(vm))            /// * Branching
+    vm.extend(mm.voc(vm))            /// * Memory
+    vm.extend(me.voc(vm))            /// * Meta compiler
+    vm.extend(db.voc(vm))            /// * Debug
+    vm.extend(os.voc(vm))            /// * OS
+    vm.extend([                      /// * last fence
+        new Prim("boot", "os", c=>{  /// * purge all user words (i.e. upto 'boot')
+            let b = vm.find("boot")
+            purge(vm.dict, b, b)
+            vm.reset()
+        })
+	])
+}
+///
+/// interfaces to Javascript
+///
 export { default as embed_forth } from './embed.js'   ///< embedded Forth listener
 export function Forth(output=console.log) {
     if (!(this instanceof Forth)) return new Forth(output);
@@ -20,7 +48,7 @@ export function Forth(output=console.log) {
     let vm   = new VM(io)                  ///< create VM instance
     let exec = (cmd)=>{                    ///< outer interpreter
         if (vm.dict.length==0) {           /// * lazy loading
-            dict.init(vm)                  /// * construct dict now
+            dict_setup(vm)                 /// * construct dict now
         }
         cmd.split("\n").forEach(r=>{       /// * multi-line input
             vm.tib(r)                      /// * capture into TIB
