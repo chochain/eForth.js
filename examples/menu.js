955ca8c2d004f4c8ab61a25430246939170edfde
gnii <chochain@yahoo.com>
2022-12-14 15:37:00 +0800

    ~/examples/menu.js use const

--- examples/menu.js
+++ examples/menu.js
@@ -6,50 +6,47 @@ const $ = (s)=>s[0]=='#' && !s.includes(' ') ?
 ///
 /// menu system
 ///
+const SHOW = 'polygon(0 0, 0 0, 0 100%, 0% 100%)'
+const HIDE = 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)'
 function menu_open(id) {
-    $(".menu_pac").forEach(m=>{
-        console.log(m)
+    $('.menu_pac').forEach(m=>{
         let sty = m.style
         if (m.id == id) {
-            sty.opacity = 1;
-            sty.pointerEvents = "all";
+            sty.opacity = 1
+            sty.pointerEvents = 'all'
         }
         else {
-            sty.opacity = 0;
-            sty.pointerEvents = "none";
-//            menu_reset(m.id)            /// reset other menu_pac 
+            sty.opacity = 0
+            sty.pointerEvents = 'none'
+            menu_reset(m.id)            /// reset other menu_pac 
         }
     })
 }
 function menu_reset(id) {
     setTimeout(function () {
-        let mm  = $('#'+id + ' > .menu')
-        let cnt = 0
-        mm.forEach(m=>{
-            if (cnt++==0) {
-                m.style.transform = "translateX(0)"
-                m.style.clipPath  = "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
-            }
-            else {
-                m.style.transform = "translateX(100%)"
-                m.style.clipPath  = "polygon(0 0, 0 0, 0 100%, 0% 100%)"
-            }
-        })
+        let mm = $('#'+id + ' > .menu')
+        for (let i=0; i < mm.length; i++) {
+            let sty = mm[i].style
+            sty.transform = `translateX(${i ? 100 : 0}%)`
+            sty.clipPath  = i ? SHOW : HIDE
+        }
     }, 300);
 }
 function menu_next(e) {
-    let m0 = $('#'+e.target.parentNode.id).style
-    let m1 = $('#'+e.target.parentNode.nextElementSibling.id).style
-    m0.transform = "translateX(-100%)"
-    m0.clipPath  = "polygon(0 0, 0 0, 0 100%, 0% 100%)";
-    m1.transform = "translateX(0)"
-    m1.clipPath  = "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+    let p  = e.target.parentNode
+    let m0 = $('#'+p.id).style
+    let m1 = $('#'+p.nextElementSibling.id).style
+    m0.transform = 'translateX(-100%)'
+    m0.clipPath  = SHOW
+    m1.transform = 'translateX(0)'
+    m1.clipPath  = HIDE
 }
 function menu_prev(e) {
-    let m1 = $('#'+e.target.parentNode.id).style
-    let m0 = $('#'+e.target.parentNode.previousElementSibling.id).style
-    m1.transform = "translateX(100%)"
-    m1.clipPath  = "polygon(0 0, 0 0, 0 100%, 0% 100%)"
-    m0.transform = "translateX(0)"
-    m0.clipPath  = "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+    let p  = e.target.parentNode
+    let m1 = $('#'+p.id).style
+    let m0 = $('#'+p.previousElementSibling.id).style
+    m1.transform = 'translateX(100%)'
+    m1.clipPath  = SHOW
+    m0.transform = 'translateX(0)'
+    m0.clipPath  = HIDE
 }
