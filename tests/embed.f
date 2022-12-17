a4086482b5f1e41caaeda71c4f7fa89088dacdb0
gnii <chochain@yahoo.com>
2022-12-17 14:29:00 +0800

    ~/tests/embed.f clarify

--- tests/embed.f
+++ tests/embed.f
@@ -1,9 +1,9 @@
-.( <h3>\ and from remote script</h3>) cr
-.( <b>\ defined a new word</b>: xx 10 for r@ . next ; ) cr
-: xx 5 for i . next ;
-.( <b>\ see xx</b> )
+.( <h3>\ and from remote scripts here</h3>)
+.( <b>\ defined a new word xx</b><br/>: xx for i . next ; ) cr
+: xx for i . next ;
+.( <br/><b>\ see xx</b> ) cr
 see xx cr
-.( <b>xx<b> ) cr
-xx cr
-.( <b>\ list directory again</b> )
-words
+.( <b>5 xx<b> ) cr
+5 xx cr
+.( <br/><b>\ so your Forth 'app' can even start on-load</b> ) cr
+
