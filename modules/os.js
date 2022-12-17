2fb5d056e18b0b200260593cac5fcea5f37975d2
gnii <chochain@yahoo.com>
2022-12-17 09:55:52 +0800

    ~/modules/os.js push date,time (instead of log)

--- modules/os.js
+++ modules/os.js
@@ -1,24 +1,20 @@
+/// @file
+/// @brief eForth - OS functions
+///
 import { Prim } from './core.js'
-///=====================================================================
-/// @defgroup OS functions
-/// @{
-const clock = ()  =>{ return Date.now() }
-const sleep = (ms)=>(new Promise(rst=>setTimeout(rst,ms))).then(()=>{})
-const date  = ()  =>{ return (new Date()).toDateString() }
-const time  = ()  =>{ return (new Date()).toLocaleTimeString() }
 
-const js_eval = (js)=>eval(js)                              /// * dangerous, be careful!
+const clock   = ()  =>{ return Date.now() }
+const sleep   = (ms)=>(new Promise(rst=>setTimeout(rst,ms))).then(()=>{})
+const js_eval = (js)=>eval(js)                     /// * dangerous, be careful!
 
 export const voc = (vm)=>{
+    const push = (v)=>vm.ss.push(v)                /// macros
+    const pop  = ()=>{ return vm.ss.pop() }
     return [
-        /// @defgroup System ops
-        /// @{
-        new Prim("clock",    "os", c=>vm.ss.push(clock())),
-        new Prim("delay",    "os", c=>sleep(pop())),
-        new Prim("date",     "os", c=>vm.log(date()+" ")),
-        new Prim("time",     "os", c=>vm.log(time()+" ")),
-        new Prim("eval",     "os", c=>js_eval(pop())),      /// * dangerous, be careful!
-        /// @}
+        new Prim('clock', c=>push(clock())),
+        new Prim('delay', c=>sleep(pop())),
+        new Prim('date',  c=>push((new Date()).toDateString())),
+        new Prim('time',  c=>push((new Date()).toLocaleTimeString())),
+        new Prim('eval',  c=>js_eval(pop()))       /// * dangerous, be careful!
     ]
 }
-/// @}
