f77fddc5f3d7a823d25f2a48f5bb5608cd3b73df
gnii <chochain@yahoo.com>
2022-12-17 11:12:14 +0800

    ~/tests/embef.f tune output

--- tests/embed.f
+++ tests/embed.f
@@ -1,9 +1,9 @@
 .( <h3>\ and from remote script</h3>) cr
 .( <b>\ defined a new word</b>: xx 10 for r@ . next ; ) cr
-: xx 10 for r@ . next ;
+: xx 5 for i . next ;
 .( <b>\ see xx</b> )
 see xx cr
 .( <b>xx<b> ) cr
-xx
+xx cr
 .( <b>\ list directory again</b> )
 words
